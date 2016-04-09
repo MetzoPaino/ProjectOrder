c7ec9728f7d088434e3764847b9464c8b912368c
William Robinson <williamjrobinson@me.com>
2016-04-09 14:24:35 +0100

    Moved app colors to own file

---
+++ Ranked/ColorManager/ColorManager.swift
@@ -0,0 +1,72 @@
+//
+//  ColorManager.swift
+//  Project Order
+//
+//  Created by William Robinson on 09/04/2016.
+//  Copyright © 2016 William Robinson. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    public class func primaryColor() -> UIColor {
+        
+        return UIColor(red: 229/255, green: 39/255, blue: 113/255, alpha: 1.0)
+    }
+    
+    public class func secondaryColor() -> UIColor {
+        
+        return UIColor(red: 142/255, green: 226/255, blue: 200/255, alpha: 1.0)
+    }
+    
+    public class func warningColor() -> UIColor {
+        
+        return UIColor(red: 219/255, green: 33/255, blue: 66/255, alpha: 1.0)
+    }
+    
+    public class func backgroundColor() -> UIColor {
+        
+        return UIColor(red: 218/255, green: 218/255, blue: 218/255, alpha: 1.0)
+    }
+    
+    public class func headingColor() -> UIColor {
+        
+        return UIColor(red: 29/255, green: 29/255, blue: 29/255, alpha: 1.0)
+    }
+    
+    public class func subHeadingColor() -> UIColor {
+        
+        return UIColor(red: 114/255, green: 113/255, blue: 110/255, alpha: 1.0)
+    }
+    
+    public class func titleColor() -> UIColor {
+        
+        return UIColor(red: 31/255, green: 31/255, blue: 31/255, alpha: 1.0)
+    }
+    
+    public class func secondColor() -> UIColor {
+        
+        return UIColor(red: 131/255, green: 47/255, blue: 85/255, alpha: 1.0)
+    }
+    
+    public class func thirdColor() -> UIColor {
+        
+        return UIColor(red: 92/255, green: 54/255, blue: 71/255, alpha: 1.0)
+    }
+    
+    public class func loserColor() -> UIColor {
+        
+        return UIColor(red: 59/255, green: 59/255, blue: 59/255, alpha: 1.0)
+    }
+    
+    public class func blockColor() -> UIColor {
+        
+        return UIColor(red: 29/255, green: 29/255, blue: 29/255, alpha: 1.0)
+    }
+    
+    public class func sortColor() -> UIColor {
+        
+        return UIColor(red: 31/255, green: 31/255, blue: 31/255, alpha: 1.0)
+    }
+}
