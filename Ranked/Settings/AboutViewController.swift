a48ca1337f8b40e0b0dc09fbd30a98e50a30aa43
William Robinson <williamjrobinson@me.com>
2016-05-22 15:09:53 +0100

    Improved UI on about screen

--- Ranked/Settings/AboutViewController.swift
+++ Ranked/Settings/AboutViewController.swift
@@ -13,9 +13,11 @@ class AboutViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var madeByLabel: UILabel!
     @IBOutlet weak var person1ImageView: UIImageView!
+    @IBOutlet weak var person1ImageViewWidthConstraint: NSLayoutConstraint!
     @IBOutlet weak var person1Label: UILabel!
     @IBOutlet weak var andLabel: UILabel!
     @IBOutlet weak var person2ImageView: UIImageView!
+    @IBOutlet weak var person2ImageViewWidthConstraint: NSLayoutConstraint!
     @IBOutlet weak var person2Label: UILabel!
     @IBOutlet weak var versionLabel: UILabel!
     
@@ -37,27 +39,61 @@ class AboutViewController: UIViewController {
 
     func styleView() {
         
-        person1ImageView.layer.cornerRadius = 50 / 2
+        person1ImageView.layer.cornerRadius = person1ImageViewWidthConstraint.constant / 2
         person1ImageView.clipsToBounds = true
-        person2ImageView.layer.cornerRadius = 50 / 2
+        person2ImageView.layer.cornerRadius = person2ImageViewWidthConstraint.constant / 2
         person2ImageView.clipsToBounds = true
         
         let williamString = "William Robinson"
+        let williamLink = "@metzopaino"
         let robynString = "Robyn Nevison"
-        
+        let robynLink = "@greenseaweed"
+
         let order = Int(arc4random_uniform(UInt32(2)))
-                
+        
+        let nameAttributes = [NSForegroundColorAttributeName: UIColor.headingColor(), NSFontAttributeName: UIFont.systemFontOfSize(22, weight: UIFontWeightRegular)]
+        let linkAttributes = [NSForegroundColorAttributeName: UIColor.primaryColor(), NSFontAttributeName: UIFont.systemFontOfSize(16, weight: UIFontWeightLight)]
+        let breakString = NSAttributedString(string: "\n")
+
         switch order {
         case 0:
-            person1Label.text = williamString
+            
+            let topNameString = NSAttributedString(string: williamString, attributes: nameAttributes)
+            let bottomNameString = NSAttributedString(string: robynString, attributes: nameAttributes)
+            let topLinkString = NSAttributedString(string: williamLink, attributes: linkAttributes)
+            let bottomLinkString = NSAttributedString(string: robynLink, attributes: linkAttributes)
+            
+            let topString = NSMutableAttributedString(attributedString: topNameString)
+            topString.appendAttributedString(breakString)
+            topString.appendAttributedString(topLinkString)
+            
+            let bottomString = NSMutableAttributedString(attributedString: bottomNameString)
+            bottomString.appendAttributedString(breakString)
+            bottomString.appendAttributedString(bottomLinkString)
+            
+            person1Label.attributedText = topString
             person1ImageView.image = UIImage(named: "William")
-            person2Label.text = robynString
+            person2Label.attributedText = bottomString
             person2ImageView.image = UIImage(named: "Robyn")
 
         default:
-            person1Label.text = robynString
+            
+            let topNameString = NSAttributedString(string: robynString, attributes: nameAttributes)
+            let bottomNameString = NSAttributedString(string: williamString, attributes: nameAttributes)
+            let topLinkString = NSAttributedString(string: robynLink, attributes: linkAttributes)
+            let bottomLinkString = NSAttributedString(string: williamLink, attributes: linkAttributes)
+            
+            let topString = NSMutableAttributedString(attributedString: topNameString)
+            topString.appendAttributedString(breakString)
+            topString.appendAttributedString(topLinkString)
+            
+            let bottomString = NSMutableAttributedString(attributedString: bottomNameString)
+            bottomString.appendAttributedString(breakString)
+            bottomString.appendAttributedString(bottomLinkString)
+            
+            person1Label.attributedText = topString
             person1ImageView.image = UIImage(named: "Robyn")
-            person2Label.text = williamString
+            person2Label.attributedText = bottomString
             person2ImageView.image = UIImage(named: "William")
         }
 
